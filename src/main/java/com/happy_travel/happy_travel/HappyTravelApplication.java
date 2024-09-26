2fb90781beba35e125d3d0a50aacac24d4814659
MAlexGG <alexandragalarzag@gmail.com>
2024-09-26 18:51:13 +0200

    happy travel init

---
+++ src/main/java/com/happy_travel/happy_travel/HappyTravelApplication.java
@@ -0,0 +1,13 @@
+package com.happy_travel.happy_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happy_travel/happy_travel/HappyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy_travel.happy_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
