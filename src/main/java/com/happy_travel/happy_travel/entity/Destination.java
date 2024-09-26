e376e952137773e696ae4231bb5b64744332f95f
MAlexGG <alexandragalarzag@gmail.com>
2024-09-26 19:09:00 +0200

    destination entity

---
+++ src/main/java/com/happy_travel/happy_travel/entity/Destination.java
@@ -0,0 +1,35 @@
+package com.happy_travel.happy_travel.entity;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "destinations")
+public class Destination {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @NonNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "La descripción no puede estar vacía")
+    @NonNull
+    @Column(nullable = false)
+    private String description;
+
+    @NotBlank(message = "La imagen no puede estar vacía")
+    @NonNull
+    @Column(nullable = false)
+    private String image;
+    
+}
