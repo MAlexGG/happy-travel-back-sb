89d45e36ccdbac7074e27c980ef2eb6b9e466986
MAlexGG <alexandragalarzag@gmail.com>
2024-09-26 20:29:09 +0200

    get all users

--- src/main/java/com/happy_travel/happy_travel/controller/UserController.java
+++ src/main/java/com/happy_travel/happy_travel/controller/UserController.java
@@ -9,12 +9,15 @@ import com.happy_travel.happy_travel.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -30,6 +33,11 @@ public class UserController {
        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK); 
+    }
+    
     @PostMapping()
     public ResponseEntity<User> saveUser(@Valid @RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
